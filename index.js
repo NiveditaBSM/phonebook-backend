c0f6c8a036079412d7fa567195594babad3b0bef
ivin <niveditamagdum2015@gmail.com>
2024-10-16 07:02:30 +0530

    forDeploy after ui build

--- index.js
+++ index.js
@@ -2,16 +2,10 @@ const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
 
+//---------------------------------------------
+// Defining/configuring middle-ware
+//---------------------------------------------
 
-// Defining custom middle-ware-----------------------------------
-
-const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-}
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
@@ -26,14 +20,17 @@ morgan.token('body', (request, response) => {
     return JSON.stringify(body)
 }, { immediate: true })
 
-// Defining helper functions--------------------------------------
+//---------------------------------------------
+// Defining helper functions
+//---------------------------------------------
 
 const generateID = () => {
     const id = Math.round(Math.random() * 1000)
     return String(id)
 }
-
-// Declaring data related stuff-----------------------------------
+//---------------------------------------------
+// Declaring data related stuff
+//---------------------------------------------
 let persons = [
     {
         "id": "1",
@@ -56,18 +53,21 @@ let persons = [
         "number": "39-23-6423122"
     }
 ]
-
-//  Using middle ware to parse json and log requests--------------
+//---------------------------------------------------
+//  Using middle ware
+//---------------------------------------------------
 const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
 
-// Routing functions----------------------------------------------
+//---------------------------------------------
+// Routing requests
+//----------------------------------------------
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
-    console.log("get request for all resources served")
     // console.log(response)
 })
 
@@ -76,7 +76,6 @@ app.get('/api/info', (request, response) => {
     const currentTime = new Date()
     response.send(`<p>Phonebook has info for ${length} people </p> 
         <p> ${currentTime.toString()}</p>`)
-    console.log("get request for api info served")
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -86,11 +85,10 @@ app.get('/api/persons/:id', (request, response) => {
     console.log(record)
     if (!record) {
         response.status(404).end()
-        console.log(`requested record with id ${id} not in the database`)
+
     } else {
         response.json(record)
         // console.log(response)
-        console.log(`requested record provided`)
     }
 })
 
@@ -99,34 +97,34 @@ app.delete('/api/persons/:id', (request, response) => {
 
     persons = persons.filter(person => person.id !== id)
     response.status(204).end()
-    console.log(`Deleted record id: ${id}`)
+
 })
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
     const isPresent = persons.find(person => person.name === body.name)
-    //console.log(isPresent)
+
     if ((!body.name) || (!body.number)) {
         response.status(400).json({
             error: 'name and number are compulsory field'
         })
-        console.log("error: name or number not provided")
+
     } else if (isPresent) {
         response.status(400).json({
             error: 'name must be unique'
         })
-        console.log("error: name not unique")
     } else {
         body.id = generateID()
         persons = persons.concat(body)
         response.json(body)
-        console.log("record added")
     }
 
 })
 
 app.use(unknownEndpoint)
 
-//  Initiating application-------------------------------------------
+//---------------------------------------------
+//  Initiating application
+//---------------------------------------------
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`Server started listening on port: ${PORT}`))
