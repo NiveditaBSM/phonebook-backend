fbf548e1cee181e6ca1c9e6b85a0f74895dd7337
ivin <niveditamagdum2015@gmail.com>
2024-10-16 10:35:55 +0530

    added mongo db atlas connection

--- index.js
+++ index.js
@@ -94,9 +94,16 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
+    const isPresent = persons.find(person => person.id === id)
+    if (isPresent) {
+        persons = persons.filter(person => person.id !== id)
+        response.status(204).end()
+    } else {
+        response.status(400).json({
+            error: 'record not on server'
+        })
+    }
 
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
 
 })
 
---
+++ mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log("provide password")
+    process.exit(0)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://niveditamagdum2015:${password}@practice.6gl40.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=practice`
+
+mongoose.connect(url)
+
+mongoose.set('strictQuery', false)
+
+const personSchema = new mongoose.Schema(
+    {
+        name: String,
+        number: String
+    }
+)
+
+const Person = mongoose.model('person', personSchema)
+
+if (process.argv.length === 5) {
+    const person = new Person(
+        {
+            name: process.argv[3],
+            number: process.argv[4]
+        }
+    )
+
+    person.save().then(result => {
+        console.log("record saved")
+        mongoose.connection.close()
+    })
+}
+
+if (process.argv.length === 3) {
+    console.log("phonebook:")
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person.name, " ", person.number)
+        })
+        mongoose.connection.close()
+    })
+}
